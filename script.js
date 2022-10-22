39e25431e6814eb5cca4a572d9c82b0f916b7b1f
Ben Merryman <ben.merryman@gmail.com>
2022-10-22 12:14:05 -0400

    added JS file and linking

---
+++ script.js
@@ -0,0 +1,5 @@
+// build h2 element > provide w/ text
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+// append h2 after body
+document.querySelector("body").appendChild(h2);
